2922dfc54bad48ee605e1e5471c4fede34825640
Anastasiya <azentsova@list.ru>
2022-08-08 20:08:01 +0700

    1

---
+++ Задача 6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число ");
+string number = Console.ReadLine();
+Console.Clear();
+double number2 = Convert.ToDouble(number);
+double what = number2 % 2;
+if(what == 0)
+{
+    Console.Write("Число " + number2 + " четное");
+}
+else
+{
+    Console.Write("Число " + number2 + " нечетное");
+}
