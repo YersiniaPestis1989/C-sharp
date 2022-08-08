bb3a7946def65e48691a64bef18f2a7946c47a8d
Anastasiya <azentsova@list.ru>
2022-08-08 20:10:01 +0700

    1

---
+++ Задача 8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число ");
+string number = Console.ReadLine();
+Console.Clear();
+int number2 = Int32.Parse(number);
+
+int count = 2;
+if(number2 < 2) 
+{
+    Console.Write("Введите другое число");
+}
+else
+{
+    while(count <= number2)
+    {
+        Console.Write(count + " ");
+        count = count + 2;
+    }
+}
