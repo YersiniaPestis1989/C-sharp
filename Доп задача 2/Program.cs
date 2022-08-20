05c008d3336da3c2e3d9128c5b71e5c56312f91f
YersiniaPestis1989 <110245192+YersiniaPestis1989@users.noreply.github.com>
2022-08-20 11:14:33 +0700

    Update Program.cs

--- Доп задача 2/Program.cs
+++ Доп задача 2/Program.cs
@@ -3,20 +3,19 @@
     private static void Main(string[] args)
     {
         //Есть прога с бесконечным циклом. Когда пользователь вводит exit, прога заканчивается
-        Console.Clear();
-        string password = "exit";
-        Console.Write(" Введите кодовое слово ");
-        string answer = Console.ReadLine();
-        int count = 1;
-        bool eq = answer.ToLower() == password;
-        while (!eq)
-        {
-            Console.Write("Снова введите кодовое слово ");
-            Console.Clear();//как сделать так, чтобы прога бесконечно предлагала вводить пароль, а не зацикливалась выводя и тут же стирая "Введите кодовое слово"
-            count++;
-        }
+Console.Write("Введите пароль: ");
+string password = Console.ReadLine();
+Console.Clear();
+int i = 0;
+ while (password != "exit") 
+ {
+    Console.WriteLine("Попытка "+ (i+1));
+    i++;
+    Console.Write("Введите ВЕРНЫЙ пароль, чтобы прекратить: ");
+    password = Console.ReadLine();
+    Console.Clear();
+}
+Console.Write("Поздравляем, вы покинули колесо сансары!");
 
-        Console.Clear();
-        Console.Write("Поздравляем, вы покинули колесо сансары!");
     }
 }
